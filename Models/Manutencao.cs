ffaf2b0d55cb6a11eb16286b595295e2429eff0a
ECODRIVEBG <ecodriveetecbg@gmail.com>
2026-09-22 12:23:54 -0300

    Criação da classe manutenção nas models

---
+++ Models/Manutencao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EcoDriveTcc.Models
+{
+    public class Manutencao
+    {
+        public int IdManutencao { get; set; }
+        public int IdVeiculo { get; set; }
+        public int IdFuncionario { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+        // Preenchidos por JOIN para exibição no painel
+        public string TokenVeiculo { get; set; }
+        public string TipoVeiculo { get; set; }
+        public string NomePonto { get; set; }
+    }
+}
