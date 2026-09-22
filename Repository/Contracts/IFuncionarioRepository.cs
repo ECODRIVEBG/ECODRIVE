cc5bb95f9b226ff7f69e10e2b0bcd9fadab8815f
ECODRIVEBG <ecodriveetecbg@gmail.com>
2026-09-22 14:55:53 -0300

    Criamos uma interface para o funcionario

---
+++ Repository/Contracts/IFuncionarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using EcoDriveTcc.Models;
+
+namespace EcoDriveTcc.Repository.Contracts
+{
+    public interface IFuncionarioRepository
+    {
+        void Cadastrar(Funcionario funcionario);
+        Funcionario Login(string email, string senha);
+        Funcionario ObterPorId(int id);
+        List<Funcionario> ObterTodos();
+        void Atualizar(Funcionario funcionario);
+        void Excluir(int id);
+    }
+}
