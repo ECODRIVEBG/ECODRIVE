b3a26e72e3830114c4623efbaefb6cbe1d0bd869
ECODRIVEBG <ecodriveetecbg@gmail.com>
2026-09-22 12:02:23 -0300

    Criamos as classes: Ponto.cs e Veiculo.cs

---
+++ Models/Ponto.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoDriveTcc.Models
+{
+    public class Ponto
+    {
+        public int IdPonto { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Tipo { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ Models/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿namespace EcoDriveTcc.Models
+{
+    public class Veiculo
+    {
+        public int IdVeiculo { get; set; }
+        public int IdPonto { get; set; }
+        public string Tipo { get; set; }      
+        public string Status_ { get; set; }    
+        public int? NivelBateria { get; set; } 
+        public string Chave { get; set; }
+
+       
+        public string NomePonto { get; set; }
+    }
+}
