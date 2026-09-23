72246f7f56af392118876c065939b267a153eb2d
ECODRIVEBG <ecodriveetecbg@gmail.com>
2026-09-22 22:15:48 -0300

    Adicionamos a proteção aos formulários

---
+++ Libraries/Middleware/ValidateAntiForgeryTokenMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+
+namespace EcoDriveTcc.Libraries.Middleware
+{
+    public class ValidateAntiForgeryTokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IAntiforgery _antiforgery;
+
+        public ValidateAntiForgeryTokenMiddleware(RequestDelegate next, IAntiforgery antiforgery)
+        {
+            _next = next;
+            _antiforgery = antiforgery;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (HttpMethods.IsPost(context.Request.Method))
+            {
+                await _antiforgery.ValidateRequestAsync(context);
+            }
+
+            await _next(context);
+        }
+    }
+}
